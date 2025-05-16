dc9c2960eacf3c386760456c8ba31d85d9b792e2
Kundan Paudel <181020254+kundanpdl@users.noreply.github.com>
2025-05-16 13:27:53 -0600

    frontend dependencies installed, some skeleton pages created

--- backend/src/controllers/messageController.js
+++ backend/src/controllers/messageController.js
@@ -1,4 +1,4 @@
-import User from "../models/userModel";
+import User from "../models/userModel.js";
 import Message from "../models/messageModel.js";
 import { v2 as cloudinary } from "cloudinary";
 
