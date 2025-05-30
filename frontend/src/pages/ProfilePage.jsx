5df7aa66c526964c2fda0b250657313fa2b73847
Kundan Paudel <181020254+kundanpdl@users.noreply.github.com>
2025-05-30 11:31:55 -0600

    chat page and profile page ui fix

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -23,9 +23,9 @@ const ProfilePage = () => {
   };
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-200 rounded-xl p-6 space-y-8">
           <div className="text-center">
             <h1 className="text-2xl font-semibold ">Profile</h1>
             <p className="mt-2">Your profile information</p>
